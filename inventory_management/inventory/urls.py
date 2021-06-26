f5a34e5a1cd6ddcc4fbeec353d9c86598353925c
attabou <attabousimo@hotmail.fr>
2021-06-26 19:12:05 +0100

    display items

--- inventory_management/inventory/urls.py
+++ inventory_management/inventory/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
 from django.urls.resolvers import URLPattern
-from .views import index
+from .views import index , display_laptops , display_desktops  , display_mobiles
 
 urlpatterns = [
     
-    url(r'^$' , index , name='index')
-
+    url(r'^$' , index , name='index') ,
+    
+    url(r'^display_laptops$' , display_laptops , name='display_laptops') ,
+    
+    url(r'^display_desktops$' , display_desktops , name='display_desktops') ,
+    
+    url(r'^display_mobiles$' , display_mobiles , name='display_mobiles')
+    
 
 ]
