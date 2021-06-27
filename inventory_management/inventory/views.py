9fcfbaefc9586c2310c61905738fa9545f3562b5
attabou <attabousimo@hotmail.fr>
2021-06-27 19:13:20 +0100

    import_exp_excel

--- inventory_management/inventory/views.py
+++ inventory_management/inventory/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
 from .models import *
+
+from .resources import ProductResource
+from django.contrib import messages
+from tablib import Dataset
+from django.http import HttpResponse
+
+def simple_upload(request):
+    if request.method  == 'POST':
+        productResource = ProductResource()
+        dataset = Dataset
+        new_product = request.FILES['myfile']
+        
+        if not new_product.name.endswith('xlsx'):
+            messages.info(request ,'wrong format')
+            return render(request , 'uplated.html')
+        imported_data = dataset.load(new_product.read(),format='xlsx')
+        for data in imported_data:
+            value = Product(
+                data[0],
+                data[1],
+                data[2],
+                data[3],
+                data[4],
+                data[5],
+                data[6]
+                )
+            value.save()
+    return render(request , 'uplated.html')
+        
+            
+        
+
+
 # Create your views here.
 
 def index(request):
